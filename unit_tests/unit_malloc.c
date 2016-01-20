3c8a4d9f6d5eab25033e5da9e14411c4fe170712
Matthieu ARENE <marene@e3r9p11.42.fr>
2016-01-20 17:06:18 +0100

    oups, oublis des tests unitaires

---
+++ unit_tests/unit_malloc.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   unit_malloc.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marene <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/01/20 16:06:32 by marene            #+#    #+#             */
+/*   Updated: 2016/01/20 16:11:16 by marene           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include "unit_test.h"
+#include "libft.h" // On sait jamais...
+
+int		unit_malloc(void)
+{
+	int*	intPtr;
+	int		max = 4;
+	int		i;
+
+	intPtr = malloc(sizeof(int) * max);
+	printf("______ %p ______\n", intPtr);
+	if (intPtr != NULL)
+	{
+		i = 0;
+		while (i < max)
+		{
+			intPtr[i] = max * i;
+			++i;
+		}
+		i = 0;
+		while (i < max)
+		{
+			printf("%p [%d]\t%d\n", intPtr, i, intPtr[i]);
+			++i;
+		}
+	}
+	return (UNIT_NOK);
+}
