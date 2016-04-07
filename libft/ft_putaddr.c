bd3bd032d185b9120616b361cd9957c5e057f3fd
Matthieu ARENE <marene@e1r1p4.42.fr>
2016-04-07 17:21:33 +0200

    changed put_addr so it outputs hex letters in uppercase. wow, such feature, much change. wow.

--- libft/ft_putaddr.c
+++ libft/ft_putaddr.c
@@ -6,7 +6,7 @@
 /*   By: marene <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/11 18:37:04 by marene            #+#    #+#             */
-/*   Updated: 2016/02/24 18:47:40 by marene           ###   ########.fr       */
+/*   Updated: 2016/04/07 15:26:22 by marene           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void			putaddr(unsigned long int n, int fd)
 	int				i;
 	char			base16[16];
 
-	ft_strcpy(base16, "0123456789abcdef");
+	ft_strcpy(base16, "0123456789ABCDEF");
 	ft_memset(res, '0', 18);
 	res[0] = '0';
 	res[1] = 'x';
